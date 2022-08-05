248aea57130af2362ad532fe43ec05276544bc85
Diego <diegopena2022@gmail.com>
2022-08-05 11:34:46 -0700

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ var timeDisplayEl = document.querySelector("#time-display")
 
 timeDisplayEl.textContent = moment().format("MMM Do YY");
 
-var userInput = document.getElementById('task-text').value;
+var userInput = document.getElementById('task-text');
 var saveInput = document.getElementById('btn1');
 
 saveInput.addEventListener("click", function() {
-    console.log(userInput)
+    console.log(userInput.value)
 })
 
 console.log(userInput)
